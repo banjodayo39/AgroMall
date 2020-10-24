178e84b909114b555ff772999172087740e4a1ca
Banjo Dayo <banjodayo39@gmail.com>
2020-10-24 07:57:53 +0100

    add Repository and ApiService, and their various dependency

--- app/src/main/java/com/banjodayo/agromall/views/FarmerActivity.kt
+++ app/src/main/java/com/banjodayo/agromall/views/FarmerActivity.kt
@@ -1,21 +1,38 @@
 package com.banjodayo.agromall.views
 
+import android.app.Activity
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.banjodayo.agromall.R
+import com.banjodayo.agromall.databinding.ActivityFarmerBinding
+import com.banjodayo.agromall.utils.Status
+import com.banjodayo.agromall.viewmodel.FarmerViewModel
+import org.koin.android.ext.android.inject
 
 class FarmerActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityFarmerBinding
+    private  val viewModel : FarmerViewModel by inject()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_farmer)
-        //setSupportActionBar(findViewById(R.id.toolbar))
+         binding = DataBindingUtil.setContentView<ActivityFarmerBinding>(
+            this, R.layout.activity_farmer)
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+        binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
+        }
+
+        viewModel.getfarmers.observe(this){
+            when(it.status){
+                Status.LOADING ->{}
+                Status.SUCCESS -> {}
+                Status.ERROR -> {}
+            }
         }
     }
 }
